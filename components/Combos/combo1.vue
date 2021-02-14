a396e71d4f73b36f0203c95e75c2677ef7f662d2
lordservet <rdservetoksuz@gmail.com>
2021-02-15 00:37:43 +0300

    s s

---
+++ components/Combos/combo1.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-combobox dense filled hide-selected outlined solo></v-combobox>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      select: [],
+      items: ["Programming3", "221", "4432", "Vuetify"],
+    };
+  },
+};
+</script>
---
+++ components/Combos/combo2.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-combobox dense filled hide-selected outlined solo></v-combobox>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      select: [],
+      items: ["Programming", "Design", "Vue", "Vuetify"],
+    };
+  },
+};
+</script>
